f482820614ab6cc2695db02fbb75d7a6bdf13a4b
maxwell86232 <55498273+maxwell86232@users.noreply.github.com>
2019-09-25 10:09:01 -0400

    Add files via upload

--- a116_traversing_turtles_maxwellmartinez.py
+++ a116_traversing_turtles_maxwellmartinez.py
@@ -11,6 +11,8 @@ turtle_colors = ["red", "blue", "green", "orange", "purple", "gold"]
 
 for s in turtle_shapes:
   t = trtl.Turtle(shape=s)
+  new_color = turtle_colors.pop()
+  t.color(new_color)
   my_turtles.append(t)
 
 #  
@@ -18,12 +20,14 @@ startx = 0
 starty = 0
 
 #
+count = 0
 for t in my_turtles:
 	t.penup()
 	t.goto(startx, starty)
 	t.pendown()
-	t.right(45)     
+	t.right(45 * count)     
 	t.forward(50)
+	count += 1
 
 #	
 	startx = t.xcor()
